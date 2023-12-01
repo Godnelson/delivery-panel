5464b6aae93c92579b0bf7ed9a31a83af6dd8f4f
Godnelson <luizgatohd@gmail.com>
2023-12-01 06:41:17 -0300

    add domain layer

---
+++ backend/src/domain/product/model.js
@@ -0,0 +1,8 @@
+class Product{
+    constructor(id, name, restaurant, photos){
+        this.id = id
+        this.name = name
+        this.restaurant = restaurant
+        this.photos = photos
+    }
+}
